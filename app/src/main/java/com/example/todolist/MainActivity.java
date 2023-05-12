d999ded983909300b5ccf5f26da655c5d42e538a
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-05-12 18:27:22 +0500

    Установим цвет фона у textViewNote.

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -62,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             int color = ContextCompat.getColor(this, colorResId);
+            textViewNote.setBackgroundColor(color);
             linearLayoutNotes.addView(view);
         }
 

1ad2f9e1b5d0c4bfd51465f7e5ece36ff2d44d81
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-05-12 17:33:56 +0500

    Создал коллекцию заметок.

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -7,20 +7,31 @@ import android.widget.LinearLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout linearLayoutNotes;
     private FloatingActionButton buttonAddNote;
+
+    private ArrayList<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            Note note = new Note(i, "Note " + i, random.nextInt(3));
+            notes.add(note);
+        }
     }
 
     private void initViews() {
         linearLayoutNotes = findViewById(R.id.linearLayoutNotes);
-        buttonAddNote = findViewById(R.id.floatingActionButton);
+        buttonAddNote = findViewById(R.id.buttonAddNote);
     }
 }

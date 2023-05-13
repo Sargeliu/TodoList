406d989ff154f8e715d878a0b60fe7b6c6427e06
sargeliu <sargeliu@mail.ru>
2023-05-14 04:04:49 +0500

    Add Database. Добавили методы, добавить заметку и удалить заметку.

---
+++ app/src/main/java/com/example/todolist/Database.java
@@ -0,0 +1,21 @@
+package com.example.todolist;
+
+import java.util.ArrayList;
+
+public class Database {
+
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    public void add(Note note) {
+        notes.add(note);
+    }
+
+    public void remove(int id) {
+        for (int i = 0; i < notes.size(); i++) {
+            Note note = notes.get(i);
+            if (note.getId() == id) {
+                notes.remove(note);
+            }
+        }
+    }
+}

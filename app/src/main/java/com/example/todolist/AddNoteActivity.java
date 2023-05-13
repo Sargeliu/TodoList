9649c92fec3989062d93b65f462e6504a5a27227
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-05-13 21:46:47 +0500

    Создаём ссылки в AddNoteActivity.

--- app/src/main/java/com/example/todolist/AddNoteActivity.java
+++ app/src/main/java/com/example/todolist/AddNoteActivity.java
@@ -5,12 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class AddNoteActivity extends AppCompatActivity {
+
+    private EditText editTextNote;
+    private RadioButton radioButtonLow;
+    private RadioButton radioButtonMedium;
+    private RadioButton radioButtonHigh;
+    private Button buttonSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+    }
+
+
+    private void initView() {
+        editTextNote = findViewById(R.id.editTextNote);
+        radioButtonLow = findViewById(R.id.radioButtonLow);
+        radioButtonMedium = findViewById(R.id.radioButtonMedium);
+        radioButtonHigh = findViewById(R.id.radioButtonHigh);
+        buttonSave = findViewById(R.id.buttonSave);
     }
 }
